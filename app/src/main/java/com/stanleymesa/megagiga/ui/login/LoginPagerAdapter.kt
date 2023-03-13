108f42153325ee8e2f7f55dad8c07ceb09d8bd5e
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2023-03-13 11:54:48 +0700

    Login UI

---
+++ app/src/main/java/com/stanleymesa/megagiga/ui/login/LoginPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.stanleymesa.megagiga.ui.login
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class LoginPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> LoginFragment()
+            else -> RegisterFragment()
+        }
+    }
+}
